da477245012f30df95fb9484195884d8eb27f97a
sltn13 <szathnanitubagus@gmail.com>
2023-10-04 17:32:02 +0700

    Tugas P2

---
+++ Pertemuan02/Praktikum/P2_1_714220059/P2_1_714220059/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_714220059
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Masukan angka Pertama");
+            int angka1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Masukan angka Kedua");
+            int angka2 = Convert.ToInt32(Console.ReadLine());
+            int penjumlahan;
+            int pengurangan;
+            int perkalian;
+            float pembagian;
+
+            penjumlahan = angka1 + angka2;
+            pengurangan = angka1 - angka2;
+            perkalian = angka1 * angka2;
+            pembagian = (float)angka1 / angka2;
+
+            Console.WriteLine($"{angka1} + {angka2} = {penjumlahan}");
+            Console.WriteLine($"{angka1} - {angka2} = {pengurangan}");
+            Console.WriteLine($"{angka1} * {angka2} = {perkalian}");
+            Console.WriteLine($"{angka1} / {angka2} = {pembagian}");
+        }
+    }
+}
