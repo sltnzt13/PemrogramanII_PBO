5044ccb13041457ce3a7e22345801d0b65465acd
sltnzt13 <szathnanitubagus@gmail.com>
2023-12-13 17:07:10 +0700

    p

---
+++ Pertemuan11/Praktikum/P10_714220059/P10_714220059/controller/Mahasiswa.cs
@@ -0,0 +1,83 @@
+﻿using P10_714220059.model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace P10_714220059.controller
+{
+    internal class Mahasiswa
+    {
+        //Memanggil class Koneksi dan membuat objek baru
+        Koneksi koneksi = new Koneksi();
+
+        //Method insert
+
+        public bool Insert(M_mahasiswa mahasiswa)
+        {
+            Boolean status = false;
+            try
+            {
+                koneksi.OpenConnection();
+                koneksi.ExecuteQuery("INSERT INTO t_mahasiswa(npm, nama, angkatan, alamat, email, nohp) VALUES('" + mahasiswa.Npm + "', '" +
+                mahasiswa.Nama + "','" + mahasiswa.Angkatan + "','" + mahasiswa.Alamat +
+                "','" + mahasiswa.Email + "','" + mahasiswa.Nohp + "')");
+                status = true;
+                MessageBox.Show("Data berhasil ditambahkan", "Informasi",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                koneksi.CloseConnection();
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Gagal Insert", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return status;
+        }
+        //Method update
+        public bool Update(M_mahasiswa mahasiswa, string npm_mhs)
+        {
+            Boolean status = false;
+            try
+            {
+                koneksi.ExecuteQuery("UPDATE t_mahasiswa SET nama='" + mahasiswa.Nama + "',"+ "angkatan='" + mahasiswa.Angkatan + "'," + "alamat='" + mahasiswa.Alamat +"'," + "email='" + mahasiswa.Email + "'," + "nohp='" + mahasiswa.Nohp + "'WHERE npm = '" + npm_mhs + "'");
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Gagal", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                koneksi.CloseConnection();
+            }
+
+            return status;
+
+        }
+        public bool Delete(string npm_mhs)
+        {
+            Boolean status = false;
+            try
+            {
+                koneksi.ExecuteQuery("DELETE FROM t_mahasiswa WHERE npm='" + npm_mhs + "'");
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Gagal", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                koneksi.CloseConnection();
+            }
+
+            return status;
+
+        }
+    }
+}
